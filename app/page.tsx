4b6e4cffa192f3b61d688d70fe4d7ea80e71705b
Aryan Srivastava <aryan.s@Aryan-Srivastava-DG957CDJXV.local>
2026-06-23 14:31:29 +0530

    chore: Migration to phaser game engine and testing the architecture

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { Navbar } from "./components/Navbar";
-export default function Home() {
-  return (
-    <div className="flex flex-col h-screen bg-gradient-to-b from-background-blue to-background-red">
-      <Navbar />
-    </div>
-  );
-}
